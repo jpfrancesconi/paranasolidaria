dec3837931eb08f0837bc9850ffd0dac5eef8ce7
jpfrancesconi <juan.francesconi@devlights.com>
2020-04-16 13:15:14 -0300

    basic structure

--- js/app.js
+++ js/app.js
@@ -1,56 +1,65 @@
-const hero = document.querySelector('.hero');
-const slider = document.querySelector('.slider');
-const logo = document.querySelector('#logo');
-const hamburguer = document.querySelector('.hamburguer');
-const headline = document.querySelector('.headline');
-const letters = document.querySelector('.letters');
+const sections = document.querySelectorAll("section");
+const bubble = document.querySelector(".bubble");
+const gradients = [
+  "linear-gradient(to right top, #bf2e65, #cc5583)",
+  "linear-gradient(to right top, #e8ba2f, #e4bd47)",
+  "linear-gradient(to right top, #3fa9f5, #62b6f1)"
+];
 
-const tl = new TimelineMax();
+const options = {
+  threshold: 0.7
+};
 
-tl.fromTo(
-  hero,
-  1,
-  { height: "0%" },
-  { height: "90%", ease: Power2.easeInOut }
-)
-/*.fromTo(
-  hero,
-  1.2,
-  { width: "100%" },
-  { width: "80%", ease: Power3.easeInOut }
-)
-.fromTo(
-  slider,
-  1.2,
-  { x: "-100%" },
-  { x: "0", ease: Power2.easeInOut },
-  "-=1.2"
-)*/
-.fromTo(
-  logo,
-  0.5,
-  { opacity: 0, x: 30 },
-  { opacity: 1, x: 0 },
-  "-=0.5"
-)
-.fromTo(
-  hamburguer,
-  0.5,
-  { opacity: 0, x: -30 },
-  { opacity: 1, x: 0 },
-  "-=0.5"
-)
-.fromTo(
-  headline,
-  0.5,
-  { opacity: 0, x: -1000 },
-  { opacity: 1, x: -100 },
-  "-=0.5"
-)
-.fromTo(
-  letters,
-  5,
-  { color: "white" },
-  { color: "orange" },
-  "-=0.5"
-);
+let observer = new IntersectionObserver(navCheck, options);
+
+function navCheck(entries){
+  entries.forEach(entry =>{
+    const className = entry.target.className;
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    const gradientIndex = entry.target.getAttribute('data-index');
+    const coords = activeAnchor.getBoundingClientRect();
+    const directions = {
+      height: coords.height,
+      width: coords.width,
+      top: coords.top,
+      left: coords.left
+    };
+    if(entry.isIntersecting){
+      bubble.style.setProperty('left', `${directions.left}px`);
+      bubble.style.setProperty('top', `${directions.top}px`);
+      bubble.style.setProperty('height', `${directions.height}px`);
+      bubble.style.setProperty('width', `${directions.width}px`);
+      bubble.style.background = gradients[gradientIndex];
+    }
+  });
+}
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  burger.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+
+    // Aniamte Links
+    navLinks.forEach((link, index)=> {
+      if(link.style.animation){
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
+    });
+    
+    // Burger Animation
+    burger.classList.toggle('toggle');
+  });
+}
+
+navSlide();
+
